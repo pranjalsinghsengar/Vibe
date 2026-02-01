e2307adb073548f2b461ba6a88419255e37971ea
pranjal singh sengar <67635283+pranjalsinghsengar@users.noreply.github.com>
2026-02-01 13:04:29 +0000

    fix

--- connect-plus/src/config/config.js
+++ connect-plus/src/config/config.js
@@ -1,5 +1,7 @@
 import React from "react";
-export const apiurl = process.env.REACT_APP_API_URL || "https://vibe.fixall.ai/xpresschat";
+export const apiurl =
+  process.env.REACT_APP_API_URL ||
+  "https://vibe-alpha-mocha.vercel.app/xpresschat";
 
 export const accessKeyId = process.env.REACT_APP_ACCESS_KEY_ID;
 export const secretAccessKey = process.env.REACT_APP_SECRET_ACCESS_KEY;
